97923090f74d6a2c51c92fa57b9dbb521c0da1c8
noelbank <bank.noel2000@gmail.com>
2020-12-08 18:15:27 +0100

    Javítás

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ let update = (delta) => {
 let render = () => {
 	let p1 = ctx.transformedPoint(0,0);
 	let p2 = ctx.transformedPoint(canvas.width,canvas.height);
-	ctx.clearRect(p1.x,p1.y,p2.x-p1.x,p2.y-p1.y);
+	ctx.clearRect(p1.x, p1.y, p2.x-p1.x ,p2.y-p1.y);
 
 	ctx.font = "5px Arial";
 
